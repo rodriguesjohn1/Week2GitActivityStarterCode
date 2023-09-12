a7bcabcc8576abf2208456a4302c927b2e845df0
John Rodrigues <rodrigues.joh@northeastern.edu>
2023-09-12 15:06:51 -0400

    implement multiply operation

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
 const result_subtract = calc.subtract(3, 2);
+const result_multiply = calc.multiply(3, 2);
 console.log(`result_add = ${result_add}`);
 console.log(`result_subtract = ${result_subtract}`);
+console.log(`result_multiply = ${result_multiply}`);
 
